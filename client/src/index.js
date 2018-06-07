ce6d53beabee9fbbf93fe3de42258ddc8ad25fea
arinalp <33346289+arinalp@users.noreply.github.com>
2018-06-07 04:23:00 +0200

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Login from './components/login';
 import Signup from './components/signup';
 import Timeline from './components/timeline';
+import Course from './components/course';
 import ForgotPassword from './components/forgotPassword';
 import ResetPassword from './components/resetPassword';
 import VerifyRegistration from './components/verifyRegistration'
@@ -22,6 +23,7 @@ ReactDOM.render(
       <Route exact path='/' component={Login}/>
       <Route path='/signup' component={Signup}/>
       <Route path='/timeline' component={Timeline}/>
+      <Route path='/course' component={Course}/>
       <Route path='/forgotPassword' component={ForgotPassword}/>
       <Route path='/resetPassword' component={ResetPassword}/>
       <Route path='/verify' component={VerifyRegistration}/>
