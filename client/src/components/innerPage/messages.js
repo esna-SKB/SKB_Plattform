6bf5bd5ac2f4a183018a8979d0bf6d71100d7417
Luka Seamus Stärk <luka.s.staerk@campus.tu-berlin.de>
2018-06-10 13:28:05 +0200

    Component Messages set

---
+++ client/src/components/innerPage/messages.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+class Threads extends React.Component {
+	constructor(props){
+	super(props);
+	}
+
+
+
+	render(){
+		return(
+			<div></div>
+			);
+	}
+}
+
+export class Messages extends React.Component {
+	constructor(props){
+	super(props);
+	this.state = {
+		user: this.props.user
+		}; 
+	}
+
+
+
+	render(){
+		return(
+			<Threads/>
+			);
+	}
+}
