feb39a9040a7308a8b6a0b609d9ca9a1dba21557
MagiiEl <magii.el@hotmail.de>
2018-06-13 21:54:30 +0200

    integration 1.2
    somehow it only comitted two changes xD

--- client/src/components/innerPage/profileedit.js
+++ client/src/components/innerPage/profileedit.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from'../img/esna.png';
-import Bell from'../img/bell-icon.png';
-import Chat from'../img/chat-icon.png';
-import '../css/timeline.css';
-import Meow from'../img/meow.png';
+import Logo from'../../img/esna.png';
+import Bell from'../../img/bell-icon.png';
+import Chat from'../../img/chat-icon.png';
+import '../../css/timeline.css';
+import Meow from'../../img/meow.png';
 
 import cookie from 'react-cookies';
-import { checkUserSession, updateTimeSec } from '../utils/userSessionHelper'; 
+import { checkUserSession, updateTimeSec } from '../../utils/userSessionHelper'; 
 
-const api = require('../api');
+const api = require('../../api');
 
 
 class Profileedit extends Component {
@@ -17,129 +17,74 @@ class Profileedit extends Component {
 		super(props);
 
 		this.state = {
-		  status: 0,
-		  firstname: '',
-		  lastname: '',
-		  email: '',
-		  isTeacher: false,
-		  isAdmin: false,
-		  description: '',
-		  iCan: '',
-		  iLearn: '',
-		  iTeach: '',
-		  website: '',
-		  errorMessage: '',
-		  infoMessage: ''
-		}	
-	
+		  user: this.props.user
+		}
+		//look at https://reactjs.org/docs/forms.html
+		//this.handleChange = this.handleChange.bind(this);
+		//this.handleSubmit = this.handleSubmit.bind(this);
 	}
 	
-	onSave(){
-		//get email
-		var token = cookie.load('userID');
-		var email;
-		console.log("token tosend fetch: "+ token);
-		fetch('userSession/'+ token+ '/email', {
-		  method: 'GET',
-		  headers: {
-			'Content-Type': 'application/json',
-		  }
+	/*handleChange(event) {
+		this.setState({value: event.target.value});
+	}
 
-		}).then(res => res.json())
-			.then(json => {
-				//get current Userdata
-				//userId = email
-				api.getUser(json.userId)
-				.then(json => { 
-					var des, can, learn, teach, web;
-					
-					//check if anything is left empty
-					if(document.getElementById("description").value.length == 0){
-						des = "";	
-					}else{
-						des = document.getElementById("description").value;
-					}
-					
-					if(document.getElementById("iCan").value.length == 0){
-						can = "";
-					}else{
-						can = document.getElementById("iCan").value;
-					}
-					
-					if(document.getElementById("iLearn").value.length == 0){
-						learn = "";
-					}else{
-						learn = document.getElementById("iLearn").value;
-					}
-					
-					if(document.getElementById("iTeach").value.length == 0){
-						teach = "";	
-					}else{
-						teach = document.getElementById("iTeach").value;
-					}
-				
-					if(document.getElementById("website").value.length == 0){
-						web = ""	
-					}else{
-						web = document.getElementById("website").value;
-					}
-					
-					api.updateUser(json.email, json.firstname, json.lastname, json.email, json.isTeacher, json.isAdmin, json.isValide, des, can, learn, teach,web );
-			
-				});
-			
-			});
+	handleSubmit(event) {
+		alert('A name was submitted: ' + this.state.value);
+		event.preventDefault();
+	}*/
+	
+	onSave(){
+		var des, can, learn, teach, web;
+		
+		//check if anything is left empty
+		if(document.getElementById("description").value.length == 0){
+			des = "";	
+		}else{
+			des = document.getElementById("description").value;
+		}
+		
+		if(document.getElementById("iCan").value.length == 0){
+			can = "";
+		}else{
+			can = document.getElementById("iCan").value;
+		}
+		
+		if(document.getElementById("iLearn").value.length == 0){
+			learn = "";
+		}else{
+			learn = document.getElementById("iLearn").value;
+		}
+		
+		if(document.getElementById("iTeach").value.length == 0){
+			teach = "";	
+		}else{
+			teach = document.getElementById("iTeach").value;
+		}
+	
+		if(document.getElementById("website").value.length == 0){
+			web = ""	
+		}else{
+			web = document.getElementById("website").value;
+		}
+		
+		api.updateUser(this.props.user.email, this.props.user.firstname, this.props.user.lastname, this.props.user.email, this.props.user.isTeacher, this.props.user.isAdmin, this.props.user.isValide, des, can, learn, teach,web );
+
 	}
 	
 	componentDidMount(){
-		//get email
-		var token = cookie.load('userID');
-		var email;
-		console.log("token tosend fetch: "+ token);
-		fetch('userSession/'+ token+ '/email', {
-		  method: 'GET',
-		  headers: {
-			'Content-Type': 'application/json',
-		  }
 
-		}).then(res => res.json())
-      .then(json => {
-			//set email in html
-			email = json.userId;
-			console.log("response is: "+ json);
-			console.log("your email is: " + email);
-
-			api.getUser(email)
-      	.then(json => {
-					//set information in html
-					document.getElementById("description").value = json.description;
-					document.getElementById("iCan").value = json.iCan;
-					document.getElementById("iLearn").value = json.iLearn;
-					document.getElementById("iTeach").value = json.iTeach;
-					document.getElementById("website").value = json.website;
-
-					//dummy values
-					var countCourses = 4;
-					var countGroups = 3;
-					document.getElementById("countCourses").innerHTML = countCourses;
-					document.getElementById("countGroups").innerHTML = countGroups;
-					//document.getElementById("countCourses2").innerHTML = countCourses;
-					//document.getElementById("countGroups2").innerHTML = countGroups;
-
-					//isadmin abfangen?
-					if(json.isTeacher){
-						document.getElementById("learn").style.display = 'none';
-						document.getElementById("can").style.display = 'none';
-						document.getElementById("teach").style.display = 'block';
+		//isadmin abfangen?
+		if(this.props.user.isTeacher){
+			document.getElementById("learn").style.display = 'none';
+			document.getElementById("can").style.display = 'none';
+			document.getElementById("teach").style.display = 'block';
 
 
-					}else{
-						document.getElementById("learn").style.display = 'block';
-						document.getElementById("can").style.display = 'block';
-						document.getElementById("teach").style.display = 'none';
-					}
-				});
-			});
+		}else{
+			document.getElementById("learn").style.display = 'block';
+			document.getElementById("can").style.display = 'block';
+			document.getElementById("teach").style.display = 'none';
+		}
 	
     }
 
@@ -167,86 +112,14 @@ class Profileedit extends Component {
 
     return (
       <div>
-      <nav className="navbar navbar-expand-sm">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="/"><img id="logo" className="logo" src={Logo} alt="Logo"/></a>
-          </div>
 
-          <form className="navbar-form navbar-center" action="/search">
-            <div className="input-group">
-              <div className="input-group-btn">
-                <button className="searchbutton btn" type="submit"></button>
-              </div>
-              <input type="text" className="searchbar form-control" placeholder="Search" name="srch-term" id="srch-term"/>
-            </div>
-          </form>
-
-          <ul className="nav navbar-nav navbar-right">
-            <li><a href="#"><img id="chat" className="icon" src={Chat} alt="Chat"/></a></li>
-            <li><a href="#"><img id="notifications" className="icon" src={Bell} alt="Bell"/></a></li>
-			<div class="btn-group">
-              <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="btnUsername">SKB User
-              </button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="/profile">Mein Profil</a>
-                <a class="dropdown-item" href="/settings">Einstellungen</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-                <div class="dropdown-divider"></div>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item text-danger" onClick={this.logout} href="/">Log Out</a>
-              </div>
-            </div>
-          </ul>
-
-        </div>
-      </nav>
-
-      <div className="background-fluid">
-        <ul class="nav nav-tabs justify-content-center col-offset-6 centered" id="mytabs" role="tablist">
-            <li class = "nav-item">
-                <a class="nav-link "  href="/timeline" role="tab">Lehrer</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" id="kurse-tab" data-toggle="tab" href="#kurse" role="tab" aria-controls="kurse" aria-selected="false">Kurse</a>
-            </li>
-            
-            
-            <li class="nav-item">
-                <a class="nav-link" id="gruppen-tab" data-toggle="tab" href="#gruppen" role="tab" aria-controls="gruppen" aria-selected="false">Gruppen</a>
-            </li>
-            
-
-            <li class="nav-item">
-                <a class="nav-link" id="kalendar-tab" data-toggle="tab" href="#kalendar" role="tab" aria-controls="kalendar" aria-selected="false">Kalendar</a>
-            </li>
-          </ul>
-      </div>
 
       <div className="container-fluid">
 
       <div className="background row">
 
-        <div className="col col-sm-3">
-          <div className="row">
-            <div className="box col text-center">
-			<div className="profilepicleft fill" ><img src={Meow} alt="meow" ></img></div>
-				<p></p><p><strong id="YourName01">SKB User</strong></p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="box col-md-6 text-center">
-              <strong id ="countCourses">2</strong><br /><small className="text-muted">Kurse</small>
-            </div>
-            <div className="box col-md-6 text-center">
-              <strong id="countGroups">5</strong><br /><small className="text-muted">Gruppen</small>
-            </div>
-          </div>
 
-        </div>
-
-        <div className="col col-sm-6">
+        <div className="col col-sm-12">
 				<div className="row box ">
 					<div className="col-sm-12">
 							<div className="row center-block">
@@ -259,31 +132,31 @@ class Profileedit extends Component {
 										<div></div>
 										<div class="form-group row newpart" >
 											<label for="description">Beschreibung</label>
-											<textarea class="form-control" id="description" rows="3" aria-describedby="Help1"></textarea>
+											<textarea class="form-control" id="description" rows="3" aria-describedby="Help1" value={this.props.user.description}></textarea>
 											<small  id="Help1" class="form-text text-muted">Erzähle etwas über dich. Suchst du nach einer Gruppe zum lernen? Was sind deine Hobbies? </small>
 										</div>
 										
 										<div class="form-group row newpart" id="can">
 											<label  for="exampleInputEmail1">ich kann:</label>
-											<input  type="text" class="form-control" id="iCan" aria-describedby="Help"></input>
+											<input  type="text" class="form-control" id="iCan" aria-describedby="Help" value={this.props.user.iCan}></input>
 											<small  id="Help" class="form-text text-muted">Erzähle, welche Sprachen du schon alle beherrschst. zum Beispiel Englisch B2 , Deutsch(Muttersprache)</small>
 										</div>
 										
 										<div class="form-group row newpart" id="learn">
 											<label  for="iLearn">ich lerne:</label>
-											<input  type="text" class="form-control" id="iLearn" aria-describedby="Help3"></input>
+											<input  type="text" class="form-control" id="iLearn" aria-describedby="Help3" value={this.props.user.iLearn}></input>
 											<small  id="Help2" class="form-text text-muted">zum Beispiel Englisch B2, Deutsch , Arabisch</small>
 										</div>
 										
 										<div class="form-group row newpart" id="teach">
 											<label  for="iTeach">ich bringe bei:</label>
-											<input  type="text" class="form-control" id="iTeach" aria-describedby="Help2"></input>
+											<input  type="text" class="form-control" id="iTeach" aria-describedby="Help2" innerHTML={this.props.user.iTeach}></input>
 											<small  id="Help2" class="form-text text-muted">zum Beispiel Englisch, Spanisch A2, Arabisch A1</small>
 										</div>
 										
 										<div class="form-group row newpart" id="teach">
-											<label  for="iTeach">Website:</label>
-											<input  type="text" class="form-control" id="website"></input>
+											<label  for="website">Website:</label>
+											<input  type="text" class="form-control" id="website" innerHTML={this.props.user.website}></input>
 										</div>
 										
 									</div>
@@ -317,18 +190,6 @@ class Profileedit extends Component {
 			
 				
 		</div>
-				
-				
-			
-				  
-			
-        <div className="col col-sm-3">
-            <div className="row">
-				<div className="box col-sm-12">
-					<h6>Kurs Leitern Kontaktieren</h6>
-				</div>
-			</div>
-        </div>
 
     </div>
 	</div>
