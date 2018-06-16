c8046f6f64307cab6108a6414852c121333a3c83
jami <jaminadarskus@gmail.com>
2018-06-16 16:11:14 +0200

    teacher Info for course

--- client/src/components/innerPage/teacherInfo.js
+++ client/src/components/innerPage/teacherInfo.js
@@ -1,27 +1,68 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import Meow from'../../img/lehrer.jpg';
+import {withRouter} from 'react-router'
 
+const api = require('../../api');
+
+
+function uniqFilterAccordingToProp(prop) {
+    if (prop)
+        return (ele, i, arr) => arr.map(ele => ele[prop]).indexOf(ele[prop]) === i
+    else
+        return (ele, i, arr) => arr.indexOf(ele) === i
+}
+
+class MyTeachers extends React.Component{
+	constructor(props){
+	super(props);
+	this.state = {
+			teacher: "",
+		};
+	}
+
+	componentDidMount(){
+		if(this.props.myEmail!=null){
+				//get teacher of the course
+					var course = window.location.pathname.split("/")[2].replace("%20", " ");
+					api.getCourse(course).then(
+						(course) => this.setState({
+							teacher: course.teacher
+						})
+					)
+		}
+	}
+	render () {
+   return (
+       <div style={{marginTop : "2em"}}>
+						 <div style={{clear: "both"}} className="contentTeacherinfo" key={this.state.teacher.email}>
+						 <img  src={Meow} alt="meow" ></img>
+						 <div> <strong><Link to={`/user/${this.state.teacher.email}`}>{this.state.teacher.firstname} {this.state.teacher.lastname}</Link></strong></div>
+						 </div>
+			</div>
+   );
+	}
+}
 
 class TeacherInfo extends React.Component {
 	constructor(props){
 	super(props);
 	this.state = {
 		myEmail: ""
-		}; 
+		};
 	}
-
-
 
 	render(){
 		return(
 			<div className="row">
 	            <div className="box col-12">
 	              <div className="box-title">
-	                Kurs Leitern Kontaktieren
+	                Kursleiter kontaktieren
 	              </div>
-	            </div>
+								<MyTeachers myEmail={this.props.user.email}/>
+							</div>
 	          </div>
 			);
 	}
 }
-
-export default TeacherInfo; 
+export default TeacherInfo;
