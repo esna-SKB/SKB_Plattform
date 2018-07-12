6e48c01167780197561224663a2b9d2b206c8d33
Ahmed Khelifi <contact@ahmedkhelifi.com>
2018-07-12 20:02:27 +0200

    mobile

--- client/src/components/innerPage/header.js
+++ client/src/components/innerPage/header.js
@@ -33,7 +33,7 @@ class Header extends React.Component {
               <button type="button" className="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 { this.props.user.firstname + " " + this.props.user.lastname }
               </button>
-              <div className="dropdown-menu dropdown-menu-right">
+              <div className="dropdown-menu dropdown-menu-right" style={{position: 'absolute'}}>
                 <Link className="dropdown-item" to={ `/user/${this.props.user.email}` }>Mein Profil</Link>
                 <Link className="dropdown-item" to={ `/settings` }>Einstellungen</Link>
                 <Link className="dropdown-item" to={ `/#` }>Something else here</Link>
