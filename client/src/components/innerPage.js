353b5441e3096c1e0949c42642243df8328afb8e
Ahmed Khelifi <contact@ahmedkhelifi.com>
2018-07-12 14:13:47 +0200

    anapssungen

--- client/src/components/innerPage.js
+++ client/src/components/innerPage.js
@@ -15,7 +15,7 @@ class InnerPage extends React.Component {
 
   render() {
     return (
-      <div className="background_illustration" style={ { backgroundColor: 'rgb(238, 239, 243)' } }>
+      <div className="background_illustration" style={ { backgroundColor: 'rgba(238, 239, 243, 0.79)' } }>
         <Header user={ this.props.user } handleLogout={ this.props.handleLogout } />
         <Body updateUser={ this.props.updateUser } user={ this.props.user } />
       </div>
